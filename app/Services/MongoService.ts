42e6295638684def586077561ae665ac150120e1
Pedro Hiram Silva S. <silvap1acbta1@gmail.com>
2024-07-20 15:59:11 -0600

    Detailed service & CRUD funcs.

--- app/Services/MongoService.ts
+++ app/Services/MongoService.ts
@@ -29,10 +29,56 @@ class MongoService {
         return this.db.collection(collectionName)
     }
 
+    // * General purpose CRUD funcs.
+
+    public async findOne(collectionName: string, query: any) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.findOne(query);
+    }
+    
+    public async findMany(collectionName: string, query: any) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.find(query).toArray();
+    }
+    
+    public async insertOne(collectionName: string, document: any) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.insertOne(document);
+    }
+    
+    public async insertMany(collectionName: string, documents: any[]) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.insertMany(documents);
+    }
+    
+    public async deleteOne(collectionName: string, query: any) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.deleteOne(query);
+    }
+    
+    public async deleteMany(collectionName: string, query: any) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.deleteMany(query);
+    }
+    
+    public async updateOne(collectionName: string, query: any, update: any) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.updateOne(query, { $set: update });
+    }
+    
+    public async updateMany(collectionName: string, query: any, update: any) {
+        const collection = await this.getCollection(collectionName);
+        return await collection.updateMany(query, { $set: update });
+    }
+
+    // * General purpose Aggregation @Func
+
     public async aggregate(collectionName: string, pipeline: any[]) {
         const collection = await this.getCollection(collectionName)
         return await collection.aggregate(pipeline).toArray()
     }
+
+    // * Custom Aggregations
 
     async getSensorLastData(dispositiveID: number, sensorID: number) {
         const agg = [
