e0e8f9136f857b3d3e53912b09178defbbd86eef
Pedro Hiram Silva S. <silvap1acbta1@gmail.com>
2024-07-21 10:56:38 -0600

    Corrección seeder

--- database/seeders/SensorTypeSeeder.ts
+++ database/seeders/SensorTypeSeeder.ts
@@ -4,14 +4,14 @@ import SensorType from 'App/Models/SensorType'
 export default class extends BaseSeeder {
   public async run () {
     await SensorType.createMany([
-      { id: 1, name: 'Ritmo', unit: 'bmp' },
+      { id: 1, name: 'Ritmo', unit: 'bpm' },
       { id: 2, name: 'Peso', unit: 'gr' },
       { id: 3, name: 'Temperatura', unit: '°C' },
       { id: 4, name: 'Alcohol', unit: '%' },
       { id: 5, name: 'Distancia', unit: 'm' },
       { id: 6, name: 'Pasos', unit: 'step' },
       { id: 7, name: 'Pantalla', unit: 'mode' },
-      { id: 8, name: 'Tiempo', unit: 'hrs' } //?Poner en "pesa" mongo
+      { id: 8, name: 'Tiempo', unit: 'hrs' } //?Poner en "Pesa" mongo
     ])
   }
 }
